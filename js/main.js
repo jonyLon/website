ae599e26e71ccc8742de8929a9fac3834752ebc7
jonyLon <masterlordgoker@gmail.com>
2024-06-04 21:15:15 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,37 @@
+
+const btn = document.getElementById('modal-button');
+
+btn.addEventListener('click', event => {
+  openModal()
+});
+
+// Get the modal element
+var modal = document.getElementById('modal');
+
+// Get the <span> element that closes the modal
+var closeBtn = document.getElementsByClassName('close')[0];
+
+// When the user clicks on <span> (x), close the modal
+closeBtn.onclick = function () {
+  modal.style.display = 'none';
+};
+
+// Open the modal
+function openModal() {
+  modal.style.display = 'block';
+}
+
+// Close the modal if the user clicks outside of it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
+};
+
+const notify = document.getElementsByClassName("notify")[0];
+const subbtn = document.getElementById("sub-btn");
+const fullname = document.getElementById("full-name");
+subbtn.addEventListener('click', event => {
+  event.preventDefault();
+  notify.textContent = `Дякую ${fullname.value} ваша заявка прийнята.`
+})
